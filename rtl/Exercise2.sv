0437311f3f090660e4701be420f62bcbbdbb8490
Fatima Farooq <ff2137@nyu.edu>
2024-03-03 19:56:06 -0500

    completed exercise2

--- rtl/Exercise2.sv
+++ rtl/Exercise2.sv
@@ -13,4 +13,17 @@ module Exercise2 (
     output logic [15:0] out
 );
 
+ logic [15:0] Value; 
+
+ always @(posedge clk)
+ begin
+ if (!nReset)
+ Value <= init;
+ else
+ Value <= {Value[14:0], Value[15] ^ Value[13] ^ Value[12] ^ Value[10]};
+ end 
+
+ assign out = Value; 
+
+
 endmodule
