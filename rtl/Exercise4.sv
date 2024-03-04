3c2dd9dbe50a8e9290f315515508ffddbb4bff3a
Fatima Farooq <ff2137@nyu.edu>
2024-03-03 22:11:33 -0500

    completed exercise4

--- rtl/Exercise4.sv
+++ rtl/Exercise4.sv
@@ -21,4 +21,21 @@ module Exercise4 (
     output logic [7:0] out
 );
 
+always_comb
+begin
+        if (cs == 0)       
+            out = 8'b00000000;
+        else if (sel == 2'b00)   
+            out = alpha;
+        else if (sel == 2'b01)   
+            out = beta;
+        else if (sel == 2'b10)   
+            out = gamma;
+        else if (sel == 2'b11)  
+            out = 8'b00000000;
+        else
+            out = 8'b00000000;   // Default case, set out to 0
+    end
+
+
 endmodule
